b2e35d33e0525d76fb59ad01464b78c723baac6d
Alexander Davenport <alexhdavenport@gmail.com>
2016-09-29 09:01:14 -0400

    Added Observatory class

--- README.rst
+++ README.rst
@@ -1,6 +1,10 @@
-mpcorbget v1.0.0a5
+mpcorbget v1.0.0a6
 =======================
 
 This is a tool for planning observations of minor planets. It pulls data from the Minor Planet Center's MPCORB.DAT and its list of observatories and calculates the geocentric and topocentric coordinates of the object.
 
 In addition to calculating coordinates, it provides an api to retrieve and utilize data from the MPC for use in other scripts.
+
+Currently the it does not support asteroids without a permanent designation. I am hoping to implement that in the future.
+
+You can type mpcorbget directly into the command line to run the QuickEphem tool script. Otherwise import mpcorbget. To see a list of objects and their attributes and methods use help("mpcorbget").
--- mpcorbget/__main__.py
+++ mpcorbget/__main__.py
@@ -2,13 +2,14 @@ import mpcorbget as mpc
 
 def main():
     """Handy dandy quick ephemeris tool"""
-    print("QuickEphem v1.0 | Code by Alex Davenport\n----------------------------------------------")
+    print("QuickEphem v1.1 | Code by Alex Davenport\n----------------------------------------------")
     asteroid = input("Asteroid Designation: ")
     observatory = input("Observatory Code: ")
     datetime = input("UTC (YYYY/MM/DD HH:MM:SS): ")
-    ast = mpc.MPCORB(asteroid)
+    ast = MPCORB(asteroid)
+    observatory = Observatory(observatory)
     geo = ast.geocentric(datetime)
-    topo = ast.topocentric(observatory, datetime)
+    topo = ast.topocentric(observatory.location, datetime)
     print("----------------------------------------------")
     print(geo)
     print()
